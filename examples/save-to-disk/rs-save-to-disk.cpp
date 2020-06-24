2f218c10403d92b3b65799fc1a14010c1c9ee339
Will Cockrum <willcockrum21@gmail.com>
2020-06-24 14:26:22 -0700

    uploading new depth saving code

--- examples/save-to-disk/rs-save-to-disk.cpp
+++ examples/save-to-disk/rs-save-to-disk.cpp
@@ -2,7 +2,7 @@
 // Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-
+//#include "example.hpp"
 #include <fstream>              // File IO
 #include <iostream>             // Terminal IO
 #include <sstream>              // Stringstreams
@@ -14,10 +14,20 @@
 // Helper function for writing metadata to disk as a csv file
 void metadata_to_csv(const rs2::frame& frm, const std::string& filename);
 
+std::tuple<uint8_t, uint8_t, uint8_t> get_texcolor(rs2::video_frame texture, rs2::texture_coordinate texcoords)
+{
+    const int w = texture.get_width(), h = texture.get_height();
+    int x = std::min(std::max(int(texcoords.u * w + .5f), 0), w - 1);
+    int y = std::min(std::max(int(texcoords.v * h + .5f), 0), h - 1);
+    int idx = x * texture.get_bytes_per_pixel() + y * texture.get_stride_in_bytes();
+    const auto texture_data = reinterpret_cast<const uint8_t*>(texture.get_data());
+    return std::tuple<uint8_t, uint8_t, uint8_t>(texture_data[idx], texture_data[idx + 1], texture_data[idx + 2]);
+}
 // This sample captures 30 frames and writes the last frame to disk.
 // It can be useful for debugging an embedded system with no display.
-int main(int argc, char * argv[]) try
+int main(int argc, char* argv[]) try
 {
+    /*
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
 
@@ -56,13 +66,78 @@ int main(int argc, char * argv[]) try
     }
 
     return EXIT_SUCCESS;
+    */
+    rs2::pointcloud pc;
+    rs2::points points;
+    rs2::pipeline pipe;
+
+    std::cout << "Fetching frame from Camera ... \n";
+    pipe.start();
+    auto frames = pipe.wait_for_frames();
+
+    auto color = frames.get_color_frame();
+    if (!color)  color = frames.get_infrared_frame();
+
+    pc.map_to(color);
+    auto depth = frames.get_depth_frame();
+    points = pc.calculate(depth);
+
+    auto vertices = points.get_vertices();
+    auto tex_coords = points.get_texture_coordinates();
+
+    int h = depth.get_height();
+    int w = depth.get_width();
+    int r, g, b;
+
+    std::ofstream myfile;
+    std::stringstream filename;
+
+    filename << "Frame_Data" << ".txt";
+    myfile.open(filename.str());
+    std::cout << "Going to write on a file ... \n";
+    //while (true) {
+    for (int i = 0; i < points.size(); i++)
+    {
+        frames = pipe.wait_for_frames();
+
+        color = frames.get_color_frame();
+        if (!color)  color = frames.get_infrared_frame();
+
+        pc.map_to(color);
+        depth = frames.get_depth_frame();
+        points = pc.calculate(depth);
+
+        vertices = points.get_vertices();
+        tex_coords = points.get_texture_coordinates();
+
+        //h = depth.get_height();
+        //w = depth.get_width();
+
+        if (vertices[i].z)
+        {
+            myfile << vertices[i].x << " " << vertices[i].y << " " << vertices[i].z;
+            std::tuple<uint8_t, uint8_t, uint8_t> current_color;
+            current_color = get_texcolor(color, tex_coords[i]);
+            r = std::get<0>(current_color);
+            g = std::get<1>(current_color);
+            b = std::get<2>(current_color);
+
+            myfile << " " << r << " " << g << " " << b << "\n";
+        }
+    }
+    //}
+    myfile.close();
+
+    system("pause");
+    return EXIT_SUCCESS;
+
 }
-catch(const rs2::error & e)
+catch (const rs2::error& e)
 {
     std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
     return EXIT_FAILURE;
 }
-catch(const std::exception & e)
+catch (const std::exception& e)
 {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
